b45aa1f3e7855e8a15f62b987035038142d615c5
Evans Shango <evanshango@github.com>
2019-08-22 12:05:30 +0300

    Account add account activity design activity_account.xml file

--- app/src/main/java/com/evans/insta/MainActivity.kt
+++ app/src/main/java/com/evans/insta/MainActivity.kt
@@ -13,30 +13,27 @@ import com.evans.insta.fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private var selectedFragment : Fragment? = null
-
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                selectedFragment = HomeFragment()
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_search -> {
-                selectedFragment = SearchFragment()
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_add -> {
                 return@OnNavigationItemSelectedListener true
             }R.id.nav_notifications -> {
-                selectedFragment = NotificationFragment()
+            moveToFragment(NotificationFragment())
+            return@OnNavigationItemSelectedListener true
             }R.id.nav_profile -> {
-                selectedFragment = ProfileFragment()
+            moveToFragment(ProfileFragment())
+            return@OnNavigationItemSelectedListener true
             }
         }
-        if(selectedFragment != null){
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container, selectedFragment!!)
-                .commit()
-        }
+
         false
     }
 
@@ -48,10 +45,13 @@ class MainActivity : AppCompatActivity() {
 
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, HomeFragment())
-            .addToBackStack(null)
-            .commit()
+        moveToFragment(HomeFragment())
+    }
+
+    private fun  moveToFragment(fragment : Fragment){
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.addToBackStack(null)
+        fragmentTrans.commit()
     }
 }
--- app/src/main/java/com/evans/insta/fragments/ProfileFragment.kt
+++ app/src/main/java/com/evans/insta/fragments/ProfileFragment.kt
@@ -1,23 +1,21 @@
 package com.evans.insta.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.evans.insta.AccountActivity
 
 import com.evans.insta.R
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class ProfileFragment : Fragment() {
 
     override fun onCreateView(
@@ -25,7 +23,13 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        view.btn_edit_profile.setOnClickListener {
+            startActivity(Intent(context, AccountActivity::class.java))
+        }
+
+        return view
     }
 
 
